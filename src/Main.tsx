292c6841351f2a61279520031588091d6b8eb977
Wisse <wissehes@gmail.com>
2022-09-03 16:24:28 +0200

    Add react-query provider

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 export default function MainApp() {
+  const client = new QueryClient();
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
